b0b0ae183811e56b9e438882da1979e77ae49f3f
luanrivello <luanrivello@hotmail.com>
2022-08-14 08:29:00 -0300

    Scheduled Update

--- automation/.config/automation/scheduler.sh
+++ automation/.config/automation/scheduler.sh
@@ -9,9 +9,13 @@ echo $weekday
 if [[ $weekday == 7 ]];
 then
     echo "update"
+
+    # Dotfiles Update
     git $dotrep add .
     git $dotrep status
     git $dotrep commit -m "Scheduled Update"
+    git $dotrep push origin main
+
     pacman -Syu
 fi
 
