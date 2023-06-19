db07aec848662e9fe351f96c76dc5e85ca971584
luanrivello <luanrivello@hotmail.com>
2023-06-19 10:13:47 -0300

    cd on quit

--- eminence/.config/nnn/nnn.sh
+++ eminence/.config/nnn/nnn.sh
@@ -10,7 +10,7 @@ export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SO
 #export NNN_OPENER="nuke"
 export NNN_FIFO="/tmp/nnn.fifo"
 export NNN_TRASH=0
-export NNN_TMPFILE="/tmp/.lastd" #cd on quit
+#export NNN_TMPFILE="/tmp/.lastd" #cd on quit
 export NNN_LOCKER='neo -sac yellow --charset=braille -S6 -G5 -b2' 
 
 export LC_COLLATE="C" # hidden files on top
@@ -37,3 +37,33 @@ NNN_BMS+="c:/run/media/$USER/Storage/codes;"
 NNN_BMS+="p:/run/media/$USER/Storage/ccc;"
 NNN_BMS+="m:/run/media/$USER/Storage/multimedia;"
 export NNN_BMS
+
+n () {
+    # Block nesting of nnn in subshells
+    [ "${NNNLVL:-0}" -eq 0 ] || {
+        echo "nnn is already running"
+        return
+    }
+
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "export" and make sure not to
+    # use a custom path, i.e. set NNN_TMPFILE *exactly* as follows:
+    #      NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    # The command builtin allows one to alias nnn to n, if desired, without
+    # making an infinitely recursive alias
+    command nnn "$@"
+
+    [ ! -f "$NNN_TMPFILE" ] || {
+        . "$NNN_TMPFILE"
+        rm -f "$NNN_TMPFILE" > /dev/null
+    }
+}
